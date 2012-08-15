88b7d553860ba60dd71a02dbcf7d5071056111f6
Adrian Webb <adrian.webb@venturiscm.com>
2012-08-15 04:09:54 -0400

    Refactoring the parameters to use parameter lookup functions in the global module.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,11 +1,49 @@
 
 class puppet::default {
-  $puppet_package_ensure = 'present'
-  $puppet_service_ensure = 'stopped'
+
+  $package_ensure        = 'present'
+  $service_ensure        = 'stopped'
   $vim_puppet_ensure     = 'present'
   $puppet_module_ensure  = 'present'
+
   $manifest_file         = 'site.pp'
   $reports               = [ 'store' ]
+
   $report_emails         = {}
+
   $update_interval       = 30  # Minutes
+
+  #---
+
+  case $::operatingsystem {
+    debian, ubuntu: {
+      $package                 = 'puppet'
+      $service                 = 'puppet'
+      $vim_puppet_package      = 'vim-puppet'
+      $puppet_module_package   = 'puppet-module'
+
+      $bin                     = "/usr/bin/puppet"
+
+      $init_config             = '/etc/default/puppet'
+      $init_config_template    = 'puppet/puppet_init.conf.erb'
+      $config_dir              = '/etc/puppet'
+      $config                  = "${config_dir}/puppet.conf"
+      $config_template         = 'puppet/puppet.conf.erb'
+      $tagmail_config          = "${config_dir}/tagmail.conf"
+      $tagmail_config_template = 'puppet/tagmail.conf.erb'
+
+      $template_dir            = "${config_dir}/templates"
+      $manifest_dir            = "${config_dir}/manifests"
+
+      $module_dirs             = [ "${config_dir}/modules" ]
+
+      $report_dir              = "/var/log/puppet/reports"
+
+      $update_environment      = 'PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin'
+      $update_command          = "${bin} apply '${manifest_dir}/${manifest_file}'"
+    }
+    default: {
+      fail("The puppet module is not currently supported on ${::operatingsystem}")
+    }
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,64 +1,39 @@
 
-class puppet::params {
+class puppet::params inherits puppet::default {
 
-  include puppet::default
+  $package                 = module_param('package')
+  $package_ensure          = module_param('package_ensure')
+  $service                 = module_param('service')
+  $service_ensure          = module_param('service_ensure')
+  $vim_puppet_package      = module_param('vim_puppet_package')
+  $vim_puppet_ensure       = module_param('vim_puppet_ensure')
+  $puppet_module_package   = module_param('puppet_module_package')
+  $puppet_module_ensure    = module_param('puppet_module_ensure')
 
-  #-----------------------------------------------------------------------------
-  # General configurations
+  #---
 
-  if $::hiera_ready {
-    $puppet_package_ensure = hiera('puppet_package_ensure', $puppet::default::puppet_package_ensure)
-    $puppet_service_ensure = hiera('puppet_service_ensure', $puppet::default::puppet_service_ensure)
-    $vim_puppet_ensure     = hiera('puppet_vim_puppet_ensure', $puppet::default::vim_puppet_ensure)
-    $puppet_module_ensure  = hiera('puppet_vim_puppet_ensure', $puppet::default::puppet_module_ensure)
-    $manifest_file         = hiera('puppet_manifest_file', $puppet::default::manifest_file)
-    $reports               = hiera('puppet_reports', $puppet::default::reports)
-    $report_emails         = hiera('puppet_report_emails', $puppet::default::report_emails)
-    $update_interval       = hiera('puppet_update_interval', $puppet::default::update_interval)
-  }
-  else {
-    $puppet_package_ensure = $puppet::default::puppet_package_ensure
-    $puppet_service_ensure = $puppet::default::puppet_service_ensure
-    $vim_puppet_ensure     = $puppet::default::vim_puppet_ensure
-    $puppet_module_ensure  = $puppet::default::puppet_module_ensure
-    $manifest_file         = $puppet::default::manifest_file
-    $reports               = $puppet::default::reports
-    $report_emails         = $puppet::default::report_emails
-    $update_interval       = $puppet::default::update_interval
-  }
+  $bin                     = module_param('bin')
 
-  #-----------------------------------------------------------------------------
-  # Operating system specific configurations
+  $init_config             = module_param('init_config')
+  $init_config_template    = module_param('init_config_template')
+  $config_dir              = module_param('config_dir')
+  $config                  = module_param('config')
+  $config_template         = module_param('config_template')
+  $tagmail_config          = module_param('tagmail_config')
+  $tagmail_config_template = module_param('tagmail_config_template')
 
-  case $::operatingsystem {
-    debian, ubuntu: {
-      $os_puppet_package          = 'puppet'
-      $os_puppet_service          = 'puppet'
-      $os_vim_puppet_package      = 'vim-puppet'
-      $os_puppet_module_package   = 'puppet-module'
+  #---
 
-      $os_bin                     = "/usr/bin/puppet"
+  $template_dir            = module_param('template_dir')
+  $manifest_dir            = module_param('manifest_dir')
+  $manifest_file           = module_param('manifest_file')
+  $module_dirs             = module_param('module_dirs')
 
-      $os_init_config             = '/etc/default/puppet'
-      $os_init_config_template    = 'puppet/puppet_init.conf.erb'
-      $os_config_dir              = '/etc/puppet'
-      $os_config                  = "${os_config_dir}/puppet.conf"
-      $os_config_template         = 'puppet/puppet.conf.erb'
-      $os_tagmail_config          = "${os_config_dir}/tagmail.conf"
-      $os_tagmail_config_template = 'puppet/tagmail.conf.erb'
+  $report_dir              = module_param('report_dir')
+  $reports                 = module_param('reports')
+  $report_emails           = module_param('report_emails')
 
-      $os_template_dir            = "${os_config_dir}/templates"
-      $os_manifest_dir            = "${os_config_dir}/manifests"
-
-      $os_module_dirs             = [ "${os_config_dir}/modules" ]
-
-      $os_report_dir              = "/var/log/puppet/reports"
-
-      $os_update_environment      = 'PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin'
-      $os_update_command          = "${os_bin} apply '${os_manifest_dir}/${manifest_file}'"
-    }
-    default: {
-      fail("The puppet module is not currently supported on ${::operatingsystem}")
-    }
-  }
+  $update_environment      = module_param('update_environment')
+  $update_command          = module_param('update_command')
+  $update_interval         = module_param('update_interval')
 }
